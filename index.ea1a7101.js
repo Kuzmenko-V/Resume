5c4b856af01a810f0eae5aac48ee108fcc93221a
Kuzmenko-V <zmeywise@gmail.com>
2022-03-05 15:31:46 +0200

    Updates

---
+++ index.ea1a7101.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("header");window.onscroll=function(){window.pageYOffset>=100?e.classList.add("header-site-background"):e.classList.remove("header-site-background")};
+//# sourceMappingURL=index.ea1a7101.js.map
